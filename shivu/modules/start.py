5df2af25fa4b2b384b7727f5e15e640b1f3429b3
SeoulDroid01 <165456817+SeoulDroid01@users.noreply.github.com>
2024-03-31 22:58:30 +0530

    Update start.py

--- shivu/modules/start.py
+++ shivu/modules/start.py
@@ -46,10 +46,10 @@ async def start(update: Update, context: CallbackContext) -> None:
         """
         
         keyboard = [
-            [InlineKeyboardButton("ADD ME", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
-            [InlineKeyboardButton("SUPPORT", url=f'https://t.me/{SUPPORT_CHAT}'),
-            InlineKeyboardButton("UPDATES", url=f'https://t.me/{UPDATE_CHAT}')],
-            [InlineKeyboardButton("HELP", callback_data='help')],
+            [InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ ", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
+            [InlineKeyboardButton("☯Sᴜᴘᴘᴏʀᴛ☯", url=f'https://t.me/{SUPPORT_CHAT}'),
+            InlineKeyboardButton("☄ᴜᴘᴅᴀᴛᴇs☄", url=f'https://t.me/{UPDATE_CHAT}')],
+            [InlineKeyboardButton("ʜᴇʟᴘ", callback_data='help')],
        
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
@@ -108,10 +108,10 @@ async def button(update: Update, context: CallbackContext) -> None:
 
         
         keyboard = [
-            [InlineKeyboardButton("ADD ME", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
-            [InlineKeyboardButton("SUPPORT", url=f'https://t.me/{SUPPORT_CHAT}'),
-            InlineKeyboardButton("UPDATES", url=f'https://t.me/{UPDATE_CHAT}')],
-            [InlineKeyboardButton("HELP", callback_data='help')],
+            [InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ ", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
+            [InlineKeyboardButton("☯Sᴜᴘᴘᴏʀᴛ☯", url=f'https://t.me/{SUPPORT_CHAT}'),
+            InlineKeyboardButton("☄ᴜᴘᴅᴀᴛᴇs☄", url=f'https://t.me/{UPDATE_CHAT}')],
+            [InlineKeyboardButton("ʜᴇʟᴘ", callback_data='help')],
            
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
