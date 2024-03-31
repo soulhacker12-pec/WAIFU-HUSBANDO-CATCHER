cf04cfb23729866ee08bd22acd447b603e129d6a
SeoulDroid01 <165456817+SeoulDroid01@users.noreply.github.com>
2024-03-31 22:21:30 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -7,7 +7,7 @@ class Config(object):
     GROUP_ID = -1002073720399
     TOKEN = "6802425596:AAEN6NJCn_1y49lOLf8l07vfpjVQXtbIAo0"
     mongo_url = "mongodb+srv://zhuofan21001:9Bpb5voEiKYcnUQm@cluster0.mibzqh9.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://te.legra.ph/file/0d1c4aa40dcdce4552084.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
+    PHOTO_URL = ["https://te.legra.ph/file/9f81d51fcc0cf0284a99f.jpg"]]
     SUPPORT_CHAT = "PokemonUniteGC"
     UPDATE_CHAT = "PokemonUniteGC"
     BOT_USERNAME = "graBxWaifuRobot"
