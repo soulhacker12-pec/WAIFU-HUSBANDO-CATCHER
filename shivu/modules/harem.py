d6fbb5550a73ac169e8fe16299a1e873219fda47
SeoulDroid01 <165456817+SeoulDroid01@users.noreply.github.com>
2024-04-01 15:57:19 +0530

    Update harem.py

--- shivu/modules/harem.py
+++ shivu/modules/harem.py
@@ -46,10 +46,9 @@ async def harem(update: Update, context: CallbackContext, page=0) -> None:
 
         for character in characters:
             
-            count = character_counts[character['id']]
-            harem_message += f'\n⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋'
-            harem_message += f'\n➥ {character["id"]} | {character["name"]} ×{count}\n'
-            harem_message += f'\n⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋'
+            count = character_counts[character['id']
+            harem_message += f'\n➥ <b>˹{character["id"]}˼</b> | {character["name"]} ×{count}\n'
+           
 
 
     total_count = len(user['characters'])
