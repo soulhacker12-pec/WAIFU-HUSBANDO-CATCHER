57ce73aa00e8f75caefa4f92be54760388d30adf
SeoulDroid01 <165456817+SeoulDroid01@users.noreply.github.com>
2024-04-03 17:34:28 +0530

    Update leaderboard.py

--- shivu/modules/leaderboard.py
+++ shivu/modules/leaderboard.py
@@ -51,7 +51,7 @@ async def ctop(update: Update, context: CallbackContext) -> None:
     ])
     leaderboard_data = await cursor.to_list(length=10)
 
-    leaderboard_message = "<b>TOP 10 USERS WHO GUESSED CHARACTERS MOST TIME IN THIS GROUP..</b>\n\n┏━┅┅┄┄⟞⟦🌐⟧⟝┄┄┉┉━┓\n\n"
+    leaderboard_message = "<b>TOP 10 USERS WHO GUESSED CHARACTERS MOST TIME IN THIS GROUP..</b>\n\n┏━┅┅┄┄⟞⟦🌐⟧⟝┄┄┉┉━┓"
     
 
     for i, user in enumerate(leaderboard_data, start=1):
