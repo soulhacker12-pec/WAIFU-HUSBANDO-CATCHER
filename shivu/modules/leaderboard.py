eee918f3ed1b9ef9e29aecf1f7be7b732750ba34
SeoulDroid01 <165456817+SeoulDroid01@users.noreply.github.com>
2024-04-03 17:29:22 +0530

    Update leaderboard.py

--- shivu/modules/leaderboard.py
+++ shivu/modules/leaderboard.py
@@ -61,8 +61,8 @@ async def ctop(update: Update, context: CallbackContext) -> None:
         if len(first_name) > 10:
             first_name = first_name[:15] + '...'
         character_count = user['character_count']
-        leaderboard_message += f'┣ {i}. <a href="https://t.me/{username}"><b>{first_name}</b></a> ➾ <b>{character_count}</b>\n┗━┅┅┄┄⟞⟦🌐⟧⟝┄┄┉┉━┛'
-        
+        leaderboard_message += f'┣ {i}. <a href="https://t.me/{username}"><b>{first_name}</b></a> ➾ <b>{character_count}</b>\n'
+        leaderboard_message += f'┗━┅┅┄┄⟞⟦🌐⟧⟝┄┄┉┉━┛'
     photo_url = random.choice(PHOTO_URL)
 
     await update.message.reply_photo(photo=photo_url, caption=leaderboard_message , parse_mode='HTML')
