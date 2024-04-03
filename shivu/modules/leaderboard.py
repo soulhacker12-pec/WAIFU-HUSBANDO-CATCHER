128bb37b4c778ca8d846d420221572b89230caea
SeoulDroid01 <165456817+SeoulDroid01@users.noreply.github.com>
2024-04-03 18:33:00 +0530

    Update leaderboard.py

--- shivu/modules/leaderboard.py
+++ shivu/modules/leaderboard.py
@@ -25,7 +25,7 @@ async def global_leaderboard(update: Update, context: CallbackContext) -> None:
     ])
     leaderboard_data = await cursor.to_list(length=10)
 
-    leaderboard_message = "<b>TOP 10 GROUPS WHO GUESSED MOST CHARACTERS</b>\n\n"
+    leaderboard_message = "<b> ˹ιтz˼ | ◈ 🌍 gℓσвαℓ ℓєα∂єявσαя∂</b>\n\n"
 
     for i, group in enumerate(leaderboard_data, start=1):
         group_name = html.escape(group.get('group_name', 'Unknown'))
