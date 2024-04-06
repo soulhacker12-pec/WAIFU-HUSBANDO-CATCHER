9a9468ed4b064bfa60e2aaa486f8594023305afb
SeoulDroid01 <165456817+SeoulDroid01@users.noreply.github.com>
2024-04-06 07:50:47 +0530

    Update upload.py

--- shivu/modules/upload.py
+++ shivu/modules/upload.py
@@ -48,7 +48,7 @@ async def upload(update: Update, context: CallbackContext) -> None:
             await update.message.reply_text('Invalid URL.')
             return
 
-        rarity_map = {1: "⚪ Common", 2: "🟣 Rare", 3: "🟡 Legendary", 4: "🟢 Medium", 5: "💮 Exclusive", 6: "🫧 Special Edition", 7: "🔮 Limited Edition", 8: "🎐 Celestial", 9: "🎄 Christmas", 10: "💘 Valentine"}
+        rarity_map = {1: "⚪ Common", 2: "🟣 Rare", 3: "🟡 Legendary", 4: "🟢 Medium", 5: "💮 Exclusive", 6: "🫧 Special Edition", 7: "🔮 Limited Edition", 8: "🎐 Celestial", 9: "🎄 Christmas", 10: "💘 Valentine", 11: "[💋] X Verse }
         try:
             rarity = rarity_map[int(args[3])]
         except KeyError:
