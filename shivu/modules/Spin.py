1f5907a178719ee66050d1f40ad85c1c296a97e3
SeoulDroid01 <165456817+SeoulDroid01@users.noreply.github.com>
2024-04-11 15:01:06 +0530

    Update Spin.py

--- shivu/modules/Spin.py
+++ shivu/modules/Spin.py
@@ -28,12 +28,8 @@ async def deduct_charms(user_id, amount):
         if current_charms >= amount:
             r.hincrby(user_info_key, 'charm', -amount)  # Deduct the specified amount of charms
         else:
-            
-            # For example, send a message to the user or take appropriate action
-            pass
-    else:
-        # Handle if user has no charm data
-        pass
+            # Handle if user doesn't have enough charms
+            raise ValueError('Insufficient charms')
 
 async def spin(update: Update, context: CallbackContext) -> None:
     try:
@@ -50,21 +46,37 @@ async def spin(update: Update, context: CallbackContext) -> None:
             await update.message.reply_text('You can only spin up to 10 times.')
             return
 
+        # Check if the user has sufficient charms for the spin
+        user_id = update.effective_user.id
+        charms_needed = 200 * spin_count
+        sufficient_charms = await check_sufficient_charms(user_id, charms_needed)
+
+        if not sufficient_charms:
+            await update.message.reply_text('You do not have enough charms for this spin.')
+            return
+
         all_waifus = await collection.find({}).to_list(length=None)
         random.shuffle(all_waifus)
         waifus = all_waifus[:spin_count]
 
         if waifus:
             # Add the waifus obtained from spin to the user's collection
-            user_id = update.effective_user.id
             await add_waifu_to_user(user_id, waifus)
 
             reply_message = "\n".join([f'˹✘˼ <b>ᴀɴɪᴍᴇ</b>: <code>{waifu["name"]}</code>\n˹✘˼ <b>ᴀɴɪᴍᴇ</b>: <code>{waifu["anime"]}</code>\n˹✘˼ <b>ʀᴀʀɪᴛʏ</b> <code>{waifu["rarity"]}</code>\n\n' for waifu in waifus])
-            await update.message.reply_text(reply_message,parse_mode='html')
+            await update.message.reply_text(reply_message, parse_mode='html')
         else:
             await update.message.reply_text('No waifus found.')
     except Exception as e:
         await update.message.reply_text(f'Error: {str(e)}')
+
+async def check_sufficient_charms(user_id, charms_needed):
+    user_info_key = f'user:{user_id}'
+    current_charms = r.hget(user_info_key, 'charm')
+    if current_charms:
+        current_charms = int(current_charms)
+        return current_charms >= charms_needed
+    return False
 
 SPIN_HANDLER = CommandHandler('spin', spin, block=False)
 application.add_handler(SPIN_HANDLER)
