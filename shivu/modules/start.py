770786fa9f5fe5878f5dbb554ff96b379a9ef735
SeoulDroid01 <165456817+SeoulDroid01@users.noreply.github.com>
2024-04-12 16:45:14 +0530

    Update start.py

--- shivu/modules/start.py
+++ shivu/modules/start.py
@@ -1,39 +1,77 @@
 import random
-from html import escape 
-
+from html import escape
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import CallbackContext, CallbackQueryHandler, CommandHandler
+import redis
 
-from shivu import application, PHOTO_URL, SUPPORT_CHAT, UPDATE_CHAT, BOT_USERNAME, db, GROUP_ID
-from shivu import pm_users as collection 
+# Redis connection setup
+r = redis.Redis(
+    host='redis-13192.c282.east-us-mz.azure.cloud.redislabs.com',
+    port=13192,
+    password='wKgGC52NC9NRhic36fDIvWh76dngPvP9')
 
+from shivu import (
+    application,
+    PHOTO_URL,
+    SUPPORT_CHAT,
+    UPDATE_CHAT,
+    BOT_USERNAME,
+    db,
+    GROUP_ID,
+    pm_users as collection,
+)
+
+async def add_waifu_to_user(user_id, waifus):
+    user = await user_collection.find_one({'id': user_id})
+    if user:
+        await user_collection.update_one({'id': user_id}, {'$push': {'characters': {'$each': waifus}}})
+    else:
+        await user_collection.insert_one({'id': user_id, 'characters': waifus})
+
+async def add_charms(user_id, amount):
+    user_info_key = f'user:{user_id}'
+    r.hincrby(user_info_key, 'charm', amount)
 
 async def start(update: Update, context: CallbackContext) -> None:
     user_id = update.effective_user.id
     first_name = update.effective_user.first_name
     username = update.effective_user.username
 
+    # Check if user was referred
+    referred_by = None
+    referral_code = context.args[0] if context.args else None  # Assuming referral code is passed as argument
+
+    if referral_code:
+        referred_by = r.get(f"referral:{referral_code}")
+
     user_data = await collection.find_one({"_id": user_id})
 
     if user_data is None:
-        
         await collection.insert_one({"_id": user_id, "first_name": first_name, "username": username})
         
+        # Store referral information if user was referred
+        if referred_by:
+            await collection.update_one({"_id": user_id}, {"$set": {"referred_by": referred_by.decode("utf-8")}})
+
+            # Add charms to referred user and referrer
+            await add_charms(user_id, 10000)
+            await add_charms(referred_by, 20000)
+
+            # Add 300 random waifus to referrer
+            all_waifus = await collection.find({}).to_list(length=None)
+            random.shuffle(all_waifus)
+            referrer_waifus = all_waifus[:300]
+            await add_waifu_to_user(referred_by, referrer_waifus)
+        
         await context.bot.send_message(chat_id=GROUP_ID, 
-                                       text=f"New user Started The Bot..\n User: <a href='tg://user?id={user_id}'>{escape(first_name)})</a>", 
+                                       text=f"New user Started The Bot..\n User: <a href='tg://user?id={user_id}'>{escape(first_name)}</a>", 
                                        parse_mode='HTML')
     else:
-        
         if user_data['first_name'] != first_name or user_data['username'] != username:
-            
             await collection.update_one({"_id": user_id}, {"$set": {"first_name": first_name, "username": username}})
 
-    
-
-    if update.effective_chat.type== "private":
-        
-        
-        caption = f"""
+    if update.effective_chat.type == "private":
+        caption = """
         ***🍂 ɢʀᴇᴇᴛɪɴɢs, ɪ'ᴍ ˹ωαιƒυ χ ᴄℓυтᴄн˼ 🥤, ɴɪᴄᴇ ᴛᴏ ᴍᴇᴇᴛ ʏᴏᴜ!
 ━━━━━━━▧▣▧━━━━━━━
 ⦾ ᴡʜᴀᴛ ɪ ᴅᴏ: ɪ sᴘᴀᴡɴ   
@@ -50,7 +88,6 @@ async def start(update: Update, context: CallbackContext) -> None:
             [InlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ", url=f'https://t.me/{SUPPORT_CHAT}'),
             InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url=f'https://t.me/{UPDATE_CHAT}')],
             [InlineKeyboardButton("ʜᴇʟᴘ", callback_data='help')],
-       
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
         photo_url = random.choice(PHOTO_URL)
@@ -64,7 +101,6 @@ async def start(update: Update, context: CallbackContext) -> None:
             [InlineKeyboardButton("SUPPORT", url=f'https://t.me/{SUPPORT_CHAT}'),
             InlineKeyboardButton("UPDATES", url=f'https://t.me/{UPDATE_CHAT}')],
             [InlineKeyboardButton("HELP", callback_data='help')],
-           
         ]
         
         reply_markup = InlineKeyboardMarkup(keyboard)
@@ -95,24 +131,23 @@ async def button(update: Update, context: CallbackContext) -> None:
 
     elif query.data == 'back':
 
-        caption = f"""
-    ***🍂 ɢʀᴇᴇᴛɪɴɢs, ɪ'ᴍ ˹ωαιƒυ χ ᴄℓυтᴄн˼ 🥤, ɴɪᴄᴇ ᴛᴏ ᴍᴇᴇᴛ ʏᴏᴜ!
+        caption = """
+        ***🍂 ɢʀᴇᴇᴛɪɴɢs, ɪ'ᴍ ˹ωαιƒυ χ ᴄℓυтᴄн˼ 🥤, ɴɪᴄᴇ ᴛᴏ ᴍᴇᴇᴛ ʏᴏᴜ!
 ━━━━━━━▧▣▧━━━━━━━
-⦾ ᴡʜᴀᴛ ɪ ᴅᴏ: ɪ sᴘᴀᴡɴ   
+⦾ ᴡʜᴀᴛ ɪ ᴅᴏ: ɪ sᴘᴀɴ   
      ωαιƒυ ɪɴ ʏᴏᴜʀ ᴄʜᴀᴛ ғᴏʀ
      ᴜsᴇʀs ᴛᴏ ɢʀᴀʙ.
 ⦾ ᴛᴏ ᴜsᴇ ᴍᴇ: ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ
      ɢʀᴏᴜᴘ ᴀɴᴅ ᴛᴀᴘ ᴛʜᴇ ʜᴇʟᴘ
      ʙᴜᴛᴛᴏɴ ғᴏʀ ᴅᴇᴛᴀɪʟs.
-━━━━━━━▧▣▧━━━━━━━*** """
-
+━━━━━━━▧▣▧━━━━━━━***
+        """
         
         keyboard = [
             [InlineKeyboardButton("ᴀᴅᴅ ᴍᴇ ", url=f'http://t.me/{BOT_USERNAME}?startgroup=new')],
             [InlineKeyboardButton("Sᴜᴘᴘᴏʀᴛ", url=f'https://t.me/{SUPPORT_CHAT}'),
             InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url=f'https://t.me/{UPDATE_CHAT}')],
             [InlineKeyboardButton("ʜᴇʟᴘ", callback_data='help')],
-           
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
 
