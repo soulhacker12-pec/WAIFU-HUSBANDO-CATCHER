89eff99e0742abb5ecbaf5e175338bdb0d158d28
SeoulDroid01 <165456817+SeoulDroid01@users.noreply.github.com>
2024-04-13 17:25:40 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -5,7 +5,7 @@ class Config(object):
     OWNER_ID = 6783092268
     sudo_users = "5162659136",5162659136,"6447641540","1782205703",1782205703,"1470441905","5746762799","6616253205","6783092268","2016154439","1861645831","666686141","6666861412",6666861412,6999153738,6616253205
     GROUP_ID = -1002073720399
-    TOKEN = "7135340603:AAFwlaTTB2cB9xU1f_LhUYoRObhqy1cUdjw"
+    TOKEN = "6802425596:AAEN6NJCn_1y49lOLf8l07vfpjVQXtbIAo0"
     mongo_url = "mongodb+srv://zhuofan21001:9Bpb5voEiKYcnUQm@cluster0.mibzqh9.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://te.legra.ph/file/9f81d51fcc0cf0284a99f.jpg","https://te.legra.ph/file/9f81d51fcc0cf0284a99f.jpg"]
     SUPPORT_CHAT = "PokemonUniteGC"
