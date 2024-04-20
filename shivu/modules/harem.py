86b8085b07a69e7ebe516ac7b2bb4b0b27a9d6c6
SeoulDroid01 <165456817+SeoulDroid01@users.noreply.github.com>
2024-04-20 08:27:03 +0530

    Update harem.py

--- shivu/modules/harem.py
+++ shivu/modules/harem.py
@@ -17,10 +17,10 @@ r = redis.Redis(
     password='wKgGC52NC9NRhic36fDIvWh76dngPvP9'
 )
 
+
 async def harem(update: Update, context: CallbackContext, page=0) -> None:
     user_id = update.effective_user.id
     user = None  # Initialize user variable
-
    
     # Define a mapping dictionary for harem modes to rarity values
     harem_mode_mapping = {
@@ -72,16 +72,18 @@ async def harem(update: Update, context: CallbackContext, page=0) -> None:
         current_characters = unique_characters[page*10:(page+1)*10]
 
         current_grouped_characters = {k: list(v) for k, v in groupby(current_characters, key=lambda x: x['anime'])} 
-
         for anime, characters in current_grouped_characters.items():
-    harem_message += f'\n\n<b>⌬ {anime} 〔{len(characters)}/{await collection.count_documents({"anime": anime})}〕</b>\n⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋'
+            harem_message += f'\n\n<b>⌬ {anime} 〔{len(characters)}/{await collection.count_documents({"anime": anime})}〕</b>\n'
 
-    for character in characters:
-        count = character_counts[character['id']]
-        # Format the ID with leading zeros if it's less than four digits
-        formatted_id = f"{int(character['id']):04d}"
-        harem_message += f'\n⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋\n➥ <b>{formatted_id}</b> | {character["rarity"][0]} | {character["name"]} ×{count}'
-        
+            for character in characters:
+                count = character_counts[character['id']]
+                # Format the ID with leading zeros if it's less than four digits
+                formatted_id = f"{int(character['id']):04d}"
+                harem_message += f'\n➥ <b>{formatted_id}</b> | {character["rarity"][0]} | {character["name"]} ×{count}'
+
+        # Add line after all characters
+        harem_message += f'\n\n⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋'
+
         total_count = len(user['characters'])
         keyboard = [[InlineKeyboardButton(f"⟭⟬ ᴄᴏɴᴄᴜʙɪɴᴇs ⌠{total_count}⌡", switch_inline_query_current_chat=f"collection.{user_id}")]]
 
@@ -141,6 +143,7 @@ async def harem(update: Update, context: CallbackContext, page=0) -> None:
         else:
             if update.message:
                 await update.message.reply_text("Your List is Empty :")
+
 
 async def harem_callback(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
