0742b58c7830c1faedd93acfb717cf8d8dd8c616
SeoulDroid01 <165456817+SeoulDroid01@users.noreply.github.com>
2024-04-21 09:17:30 +0530

    Update changetime.py

--- shivu/modules/changetime.py
+++ shivu/modules/changetime.py
@@ -26,7 +26,7 @@ async def change_time(client: Client, message: Message):
             return
 
         new_frequency = int(args[1])
-        if new_frequency < 4:
+        if new_frequency < 10:
             await message.reply_text('The message frequency must be greater than or equal to 5.')
             return
 
