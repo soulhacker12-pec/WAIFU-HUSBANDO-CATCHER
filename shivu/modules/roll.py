4c6e444c9bee52005c249068c7b530215b23edd6
SeoulDroid01 <165456817+SeoulDroid01@users.noreply.github.com>
2024-04-21 13:31:49 +0530

    Update roll.py

--- shivu/modules/roll.py
+++ shivu/modules/roll.py
@@ -5,6 +5,7 @@ import time
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler 
 import redis
+import html
 
 # Cooldown Tracking (in-memory for demonstration)
 last_roll_reward = {}  # {user_id: timestamp}
@@ -16,41 +17,36 @@ r = redis.Redis(
     password='wKgGC52NC9NRhic36fDIvWh76dngPvP9')
 
 def can_earn_reward(user_id):
-    user_info_key = f'user:{user_id}'
-    last_reward_time = r.get(user_info_key) 
+    key = f'user:{user_id}'
+    last_reward_time = r.get(key)
     if last_reward_time:
-        current_time = time.time()
-        return current_time - float(last_reward_time) >= 60
-    return True
+        cooldown_seconds = 60 - (time.time() - float(last_reward_time))
+        return False, cooldown_seconds
+    return True, 0  
+
 
 async def roll(update: Update, context: CallbackContext):
     user_id = update.effective_user.id
+    ready, cooldown_seconds = can_earn_reward(user_id)
 
-    if can_earn_reward(user_id):
+    if ready:
         roll_result = random.randint(1, 6)
         reward = random.randint(500, 2000)
-        user_info_key = f'user:{user_id}'
-        r.hset(user_info_key, 'charms', 25000)  # Initialize if the hash field doesn't exist
-     
-        # 45% chance for "better luck next time" directly
-        if random.random() < 0.45:  
+        key = f'user:{user_id}'
+        r.hset(key, 'charms', 50000)  # Initialize if needed 
+        
+        if random.random() < 0.45:  # 45% chance
             await update.message.reply_text('Better luck next time!')
         else:
-            r.hincrby(user_info_key, 'charms' , +reward)  
-
-            # Stage 1: Send the rolling dice
-            await update.message.reply_dice(emoji='🎲')  
-
-            #Stage 2: Send the result and rewards
-            keyboard = [[InlineKeyboardButton("🎲", callback_data=str(roll_result))]]
-            reply_markup = InlineKeyboardMarkup(keyboard)
-            await update.message.reply_text(f'You rolled and earned {reward} charms!', reply_markup=reply_markup)
-
-        # Set cooldown in Redis
-        r.setex(user_info_key, 60, time.time()) 
-
+            r.hincrby(key, 'charms', +reward) 
+            await update.message.reply_dice('🎲') 
+            await update.message.reply_text(
+                f'<b>You rolled and earned {reward} charms!</b>',parse_mode='html', 
+                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🎲", callback_data=str(roll_result))]])
+            )
+        r.setex(key, 60, time.time())  
     else:
-        await update.message.reply_text('Please wait before rolling again for rewards.')
+        await update.message.reply_text(f'<b>Try again in {int(cooldown_seconds)} seconds.</b>',parse_mode='html')
 
 
 # ... Your other bot setup code 
