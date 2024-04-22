4a1f683f1261f383226edd13fe176d2c0d306777
SeoulDroid01 <165456817+SeoulDroid01@users.noreply.github.com>
2024-04-22 19:05:00 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -314,7 +314,8 @@ async def send_charm_count(update: Update, context: CallbackContext) -> None:
     message = (
         f"<b>┏━┅┅┄┄⟞⟦🎐⟧⟝┄┄┉┉━┓\n"
         f"┣ ¢нαямѕ ˹𝕮𝖔𝖚𝖓𝖙˼</b> <code>➾ {formatted_charm_count}</code>\n"
-        f"┗━┅┅┄┄⟞⟦🎐⟧⟝┄┄┉┉━┛\n"#https://telegra.ph/file/9984fc1ee8bfe50d4ff30.jpg.
+        f"┗━┅┅┄┄⟞⟦🎐⟧⟝┄┄┉┉━┛\n" 
+        f"<a href='https://telegra.ph/file/9984fc1ee8bfe50d4ff30.jpg'></a>
     )
     await update.message.reply_text(message, parse_mode='html')
     LOGGER.info("Sex")
